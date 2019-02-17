b913edee753d479450767445cffd632bfad0d8c8
sheldonfar <sheldonfar@gmail.com>
2019-02-17 16:36:23 +0100

    Updates

---
+++ precache-manifest.39b3294300f526c43723be7ffcdaf9e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "05693e0ce48da0ff2010",
+    "url": "/current.ly/static/css/main.f7134747.chunk.css"
+  },
+  {
+    "revision": "05693e0ce48da0ff2010",
+    "url": "/current.ly/static/js/main.05693e0c.chunk.js"
+  },
+  {
+    "revision": "250757ce31329824a13b",
+    "url": "/current.ly/static/js/runtime~main.250757ce.js"
+  },
+  {
+    "revision": "56dd5f5d666e78b7506f",
+    "url": "/current.ly/static/js/2.56dd5f5d.chunk.js"
+  },
+  {
+    "revision": "650659c6fcc4bd8971b66aa5a18671e7",
+    "url": "/current.ly/index.html"
+  }
+];
